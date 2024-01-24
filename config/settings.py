00838e4be93e52421c8ed9f600775e19b0bf1aa2
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-01-23 21:10:40 -0600

    add movie to admin add movie model

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # start of our apps
+    'movies',
 ]
 
 MIDDLEWARE = [
