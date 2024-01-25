de794d0208ff24ba1bd0a63dc510aac59ca684ab
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-01-24 21:21:46 -0600

    Add exception handling for ObjectDoesNotExist in MovieDetailView

--- movies/views.py
+++ movies/views.py
@@ -2,6 +2,7 @@ from django.http import JsonResponse
 
 # Create your views here.
 from .models import Movie
+from django.core.exceptions import ObjectDoesNotExist
 
 def MovieListView(request):
     movies = Movie.objects.all()
@@ -10,7 +11,11 @@ def MovieListView(request):
     return JsonResponse({"movies": list(movies.values())})
 
 def MovieDetailView(request, pk):
-    movie = Movie.objects.get(pk=pk)
+    try:
+        movie = Movie.objects.get(pk=pk)
+    except ObjectDoesNotExist:
+        return JsonResponse({"message": "Movie not found"}, status=404)
+    
     return JsonResponse({"movie": {
         "title": movie.title,
         "description": movie.description,
