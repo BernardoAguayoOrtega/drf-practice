b57bde9a64751d62a91543a2fea4103822e31ac6
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-02-07 20:04:51 -0600

    Refactor movie_list and movie_detail views

--- movies/api/views.py
+++ movies/api/views.py
@@ -14,14 +14,14 @@ def movie_list(request):
         movies = Movie.objects.all()
         serializer = MovieSerializer(movies, many=True)
 
-        return Response(serializer.data)
-    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == "POST":
         serializer = MovieSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+            return JsonResponse(serializer.data, status=status.HTTP_201_CREATED)
+        return JsonResponse(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(["GET", "PUT", "DELETE"])
@@ -29,19 +29,24 @@ def movie_detail(request, pk):
     try:
         movie = Movie.objects.get(pk=pk)
     except Movie.DoesNotExist:
-        return Response({'message': 'Movie not found'}, status=status.HTTP_404_NOT_FOUND)
+        return Response(
+            {"message": "Movie not found"}, status=status.HTTP_404_NOT_FOUND
+        )
 
     if request.method == "GET":
         serializer = MovieSerializer(movie)
-        return Response(serializer.data)
-    
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == "PUT":
         serializer = MovieSerializer(movie, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data)
+            return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
     elif request.method == "DELETE":
         movie.delete()
-        return Response({"message": "Movie deleted successfully!"}, status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {"message": "Movie deleted successfully!"},
+            status=status.HTTP_204_NO_CONTENT,
+        )
