44a32a6acb6cc896ff264cf3a4c7becfdd4f465d
Bernardo Aguayo <bernardo.aguayo.ortega@gmail.com>
2024-02-26 21:59:27 -0600

    Update serializers validation and views to use the new serializers

--- movies/api/serializers.py
+++ movies/api/serializers.py
@@ -7,23 +7,16 @@ class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = "__all__"
-        
+
     id = serializers.IntegerField(read_only=True)
     title = serializers.CharField()
     description = serializers.CharField()
     year = serializers.IntegerField()
     active = serializers.BooleanField(default=True)
-    
-    def validate_title(self, value):
-        if len(value) < 2:
-            raise serializers.ValidationError("Title is too short")
-        if len(value) > 100:
-            raise serializers.ValidationError("Title is too long")
-        return value
 
     def create(self, validated_data):
         return Movie.objects.create(**validated_data)
-    
+
     def update(self, instance, validated_data):
         instance.title = validated_data.get("title", instance.title)
         instance.description = validated_data.get("description", instance.description)
@@ -31,7 +24,19 @@ class MovieSerializer(serializers.ModelSerializer):
         instance.active = validated_data.get("active", instance.active)
         instance.save()
         return instance
-    
+
     def delete(self, instance):
         instance.delete()
         return instance
+
+    def validate(self, data):
+        if data["title"] == data["description"]:
+            raise serializers.ValidationError("Title and description must be different")
+        return data
+
+    def validate_title(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError("Title is too short")
+        if len(value) > 100:
+            raise serializers.ValidationError("Title is too long")
+        return value
